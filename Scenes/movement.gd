bba2aa782303dd241bddeb2368da14ac17115d55
unknown <jreigner5040@stevenscollege.edu>
2025-06-30 21:54:04 -0400

    added ian and hopscotch image

--- Scenes/movement.gd
+++ Scenes/movement.gd
@@ -3,4 +3,10 @@ extends Sprite2D
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("forward"):
-		self.position.y -= 5
+		self.position.y -= 10
+	if Input.is_action_just_pressed("back"):
+		self.position.y += 10
+	if Input.is_action_just_pressed("left"):
+		self.position.x -= 10
+	if Input.is_action_just_pressed("right"):
+		self.position.x += 10
